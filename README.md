e08efbf09d19fc5b2d0c91ea8a485cc5fc0cba6d
omer-io <omer.jamil@rakurai.io>
2024-07-10 10:13:05 +0500

    readme and main.cpp added

---
+++ README.md
@@ -0,0 +1,10 @@
+# **Print Your Name**
+---
+## Clone this repo
+`git@github.com:omer-io/repo4.git`
+
+### Compile
+`g++ -o out main.cpp`
+
+### Execute
+`./out`
---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello peeps";
+    string your_name, your_age, your_gender;
+    cout << "Enter your name: ";
+    cin >> your_name;
+    cout << "\nYour name is: " << your_name;
+    cout << "\nEnter your age: ";
+    cin >> your_age;
+    cout << "\nYour age is " << your_age;
+    cout << "\nEnter your gender: ";
+    cin >> your_gender;
+    cout << "\nYour gender is: " << your_gender;
+
+}
